1ff0e7a77468d4c6fe2f1c4d90b7ec8ad4b37551
Jan Lehoczký <leho.cz.ky@email.cz>
2024-05-22 18:53:48 +0200

    #18 fix

--- js/svgAreaPolygonObject.js
+++ js/svgAreaPolygonObject.js
@@ -385,13 +385,8 @@ export class svgAreaPolygonObject {
       return false;
     }
     for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i].length !== arr2[i].length) {
+      if (Math.abs(arr1[i] - arr2[i]) >= this.EPSILON) {
         return false;
-      }
-      for (let j = 0; j < arr1[i].length; j++) {
-        if (Math.abs(arr1[i][j] - arr2[i][j]) >= this.EPSILON) {
-          return false;
-        }
       }
     }
     return true;
@@ -459,6 +454,18 @@ export class svgAreaPolygonObject {
       }
     }
     return isInside;
+  }
+
+  checkIfPointIsVertex(point) {
+    let isVertex = false;
+    for (let i = 0; i < this.points.length; i++) {
+      const element = this.points[i];
+      if(this.arraysAreEqual(element, point)){
+        isVertex = true;
+        break;
+      }
+    }
+    return isVertex;
   }
 
 }
