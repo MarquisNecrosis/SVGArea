40bcd621c9aeb082f5abad6dd1dbdb20e05a9b03
Jan Lehoczký <leho.cz.ky@email.cz>
2024-06-30 20:47:20 +0200

    #26 random generate polygons btn

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
-import {svgAreaCalculation} from './svgAreaCalculation.js';
+import { svgAreaCalculation } from './svgAreaCalculation.js';
 import { svgAreaIntersection } from './svgAreaIntersection.js';
-import {svgRandomGenerate} from './svgRandomGenerate.js';
+import { svgRandomGenerate } from './svgRandomGenerate.js';
 
 // Create an instance of the SvgAreaCalculation class
 const areaCalculator = new svgAreaCalculation();
-const svgRandom = new svgRandomGenerate('tutorial_svg');
 const intersectElements = new svgAreaIntersection('tutorial_svg');
 
-window.onload = function() {
+window.onload = function () {
   //svgRandom.generateRandomRectangle(10);
 
   const parentElement = document.getElementById('tutorial_svg');
@@ -19,7 +18,7 @@ window.onload = function() {
   intersectElements.polygonIntersectionInSvg(true, 'blue');
   const checkbox = document.getElementById('opacity-test');
 
-  checkbox.addEventListener('input', function() {
+  checkbox.addEventListener('input', function () {
     const elements = document.querySelectorAll('.intersect-object');
     const value = this.value;
     elements.forEach(element => {
@@ -27,46 +26,25 @@ window.onload = function() {
     });
   });
 
-  document.getElementById('copy').addEventListener('click', function() {
+  document.getElementById('copy').addEventListener('click', function () {
     const container = document.getElementById('tutorial_svg');
     var elementsToCopy = container.querySelectorAll('.area-calculate');
     var copiedContent = '';
-    elementsToCopy.forEach(function(element) {
-        copiedContent += element.outerHTML + '\n';
+    elementsToCopy.forEach(function (element) {
+      copiedContent += element.outerHTML + '\n';
     });
     navigator.clipboard.writeText(copiedContent)
-    .then(function() {
+      .then(function () {
         alert('Elements copied to clipboard!');
-    })
-    .catch(function(err) {
+      })
+      .catch(function (err) {
         alert('Failed to copy elements: ' + err);
-    });
-});
+      });
+  });
+  document.getElementById('generate-btn').addEventListener('click', function() {
+    const svgRandom = new svgRandomGenerate('tutorial_svg');
+    const numberOfRandomPolygons = document.getElementById('generate-input').value;
+    svgRandom.generateRandomRectangle(numberOfRandomPolygons);
+  });
 
-  /*
-  svgRandom.generateRandomRectangle(20);
- const totalArea = areaCalculator.areaInSvg('tutorial_svg');
- console.log(totalArea);*/
- /*
-  const totalArea = areaCalculator.areaInSvg('tutorial_svg');
-  console.log(totalArea);
-  const totalArea2 = areaCalculator.areaInSvgByGroup('tutorial_svg', '1');
-  console.log(totalArea2);
-  const totalArea3 = areaCalculator.areaInSvgByGroup('tutorial_svg', '2');
-  console.log(totalArea3);
-  const totalArea4 = areaCalculator.areaInSvgByGroup('tutorial_svg', '3');
-  console.log(totalArea4);
-
-  const totalAreaIntersect2 = areaCalculator.areaInSvgWithIntersection('tutorial_svg', svgAreaCalculation.LIT_RANDOM_POINTS);
-  console.log(totalAreaIntersect2);
-  const totalAreaIntersect3 = areaCalculator.areaInSvgWithIntersection('tutorial_svg', svgAreaCalculation.MOD_RANDOM_POINTS);
-  console.log(totalAreaIntersect3);
-  const totalAreaIntersect4 = areaCalculator.areaInSvgWithIntersection('tutorial_svg', svgAreaCalculation.HIGH_RANDOM_POINTS);
-  console.log(totalAreaIntersect4);
-  const preciseArea = areaCalculator.lazyStupidAreaCalculation('tutorial_svg');
-  console.log(preciseArea);
-  
-  intersectElements.lineIntersection(1, 1, 3, 2, 1, 4, 2, -1);
-  intersectElements.lineIntersection(-1, 0, 4, 0, 1, 4, 1, -1);
-*/
 };
